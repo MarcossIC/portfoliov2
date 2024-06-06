cb2b5e09a3478104520a81299d8b0a2f46f7ec7b
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-06-05 21:40:29 -0300

    Update bento grid, add icons and illustrations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,33 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				manrope: ['Manrope', 'sans-serif'],
+			},
+			backgroundImage: {
+				'ml-text-white': 'var(--ml-text-white)',
+			},
+			animation: {
+				meteor: "meteor 5s linear infinite",
+				"border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+			},
+			keyframes: {
+				meteor: {
+					"0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+					"70%": { opacity: 1 },
+					"100%": {
+						transform: "rotate(215deg) translateX(-500px)",
+						opacity: 0,
+					},
+				},
+				"border-beam": {
+					"100%": {
+						"offset-distance": "100%",
+					},
+				},
+			},
+		},
 	},
 	plugins: [],
 }
